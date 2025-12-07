7f31703c11b0e0f04af5ea659a52994b4d8855fb
Aaaaangla <anglali0405@gmail.com>
2025-12-07 18:03:42 +0800

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("Angla 的个人网站")
+st.subheader("Master of Information Systems | Business & Data Analyst | Architecture + Civil Dual Background")
+
+st.write("""
+Hi，我是 Frank！我目前在 Curtin University 读 MIS（Data Analytics & Visualisation），
+我拥有商业分析、建筑设计与土木工程背景。
+这是我用 Streamlit 制作的个人网站！
+""")
+
+st.header("✨ 我的技能")
+st.write("""
+- Python / R / SQL  
+- Data Analytics & Machine Learning  
+- Architecture Design + Civil Engineering  
+- Business Analysis  
+- Portfolio Management（APP Group）  
+""")
+
+st.header("📂 项目展示")
+st.write("这里可以放你做的 ML 项目、可视化、程序 demo 等。")
+
+st.header("📬 联系我")
+st.write("Email: your-email@example.com")
